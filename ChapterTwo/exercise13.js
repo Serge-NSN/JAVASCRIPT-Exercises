89114c8edf13415e4be3b1e46b4eb2b37da53b94
ntunyuserge <ntunyuserge@gmail.com>
2022-12-15 18:53:54 +0100

    Added new file

---
+++ ChapterTwo/exercise13.js
@@ -0,0 +1,9 @@
+// Declare two variables of type int. Assign to them values 5 and 10
+// respectively. Exchange (swap) their values and print them.
+
+var a = 5;
+var b = 10;
+var temp = a;
+a = b;
+b = temp;
+console.log(a,b);
---
+++ ChapterTwo/exercise9.js
@@ -0,0 +1,6 @@
+// Declare two variables of type string and assign them a value “The
+// "use" of quotations causes difficulties.” (without the outer quotes).
+// In one of the variables use quoted string and in the other do not use it.
+
+var a = "The use of quotations causes difficulties";
+var b = 'The "use" of quotations causes difficulties';

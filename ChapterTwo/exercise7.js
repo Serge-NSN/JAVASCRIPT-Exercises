4d39fda53d3466cec001afc445926e039c8c1c7d
Ntunyu Serge <ntunyuserge@gmail.com>
2022-12-15 18:58:53 +0100

    Added new file

--- ChapterTwo/exercise7.js
+++ ChapterTwo/exercise7.js
@@ -1,7 +1,7 @@
 // Declare two variables of type string with values "Hello" and "World".
 // Declare a variable of type object. Assign the value obtained of
 // concatenation of the two string variables (add space if necessary) to this
-// variable. Print the variable of type object.
+// variable. Print the variable of type object..
 
 
 const string1 = "Hello";

ec79966f4d7ae3bc407f347e7e8d59c44834dc30
ntunyuserge <ntunyuserge@gmail.com>
2022-12-15 11:53:36 +0100

    Declared string variables

---
+++ ChapterTwo/exercise7.js
@@ -0,0 +1,8 @@
+// Declare two variables of type string with values "Hello" and "World".
+// Declare a variable of type object. Assign the value obtained of
+// concatenation of the two string variables (add space if necessary) to this
+// variable. Print the variable of type object.
+
+
+const string1 = "Hello";
+const string2 = "World";
